a85931445ba12c7a1686539398cf732f7713665e
Mark Kennedy <mark@gamer-network.net>
2021-01-09 23:10:23 +0000

    GPIO -> CC map; de-bounce for noisy pots

--- fina_midi.ino
+++ fina_midi.ino
@@ -1,9 +1,7 @@
-// MIDIUSB - Version: 1.0.4
-
-
 /*
   http://www.arduino.cc/en/Tutorial/AnalogReadSerial
   https://lab.arts.ac.uk/books/physical-computing/page/using-arduino-leonardo-to-send-usb-midi-data
+  https://arduino.programmingpedia.net/en/tutorial/9406/midi-communication - midi msg reference
 */
 
 #include <MIDIUSB.h>
@@ -12,9 +10,23 @@
 #include <pitchToFrequency.h>
 #include <pitchToNote.h>
 
-int sensors[] = { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9 };
-byte last_readings[10];
+const int change_threshold = 5;
+const int deadzone = 5;
+const int sensor_timeout = 5;
+
+// number of sensors
 int s_count = 10;
+
+// combined these provide a GPIO pin -> midi channel mapping
+int sensors[] =  { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9 };
+int channels[] = { 25, 26, 27, 28, 29, 20, 21, 22, 23, 24 };
+
+// used for change detection - you don't want to output constantly
+int last_readings[10];
+
+// is this channel being actively controlled (used by debounce)
+int active[] =   {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 };
+
 char op[11] = "          ";
 int midi_channel = 0;
 
@@ -22,36 +34,65 @@ int midi_channel = 0;
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
+  // at start shouldn't spam readings so preload last_readings
+  for( int i = 0; i < s_count; i ++ ) {
+    last_readings[i] = analogRead(sensors[i]);
+  }
 }
 
-
+byte inline raw_to_midiCC(int val) {
+  return (byte) (val * 128 / 1024);
+}
 
 // the loop routine runs over and over again forever:
 void loop() {
-  byte new_readings[10];
+  int new_readings[10];
   int sensor_value = 0;
   for( int i = 0; i < s_count; i ++ ) {
     sensor_value = analogRead(sensors[i]);
-    new_readings[i] = (byte)(sensor_value * 128 / 1024);
-    
+    //new_readings[i] = (byte)(sensor_value * 128 / 1024);
+    new_readings[i] = sensor_value;
+
+    // have we moved?
+    if (new_readings[i] < last_readings[i] - change_threshold
+      || new_readings[i] > last_readings[i] + change_threshold ) {
+        // keep resetting the sensor timeout as we're definitely moving
+        active[i] = sensor_timeout;
+    } else {
+      // since the change is very small this *could* be a motionless
+      // pot and we're just seeing a bit of jitter, so we count down the active state
+      if (active[i] > 0) {
+        active[i] --;
+      }
+    }
+
+    // we only update last reading if the sensor is active, otherwise we can get stuck if pot is
+    // only ever moved slowly
+    if (active[i] > 0) {
+      last_readings[i] = new_readings[i];
+    }
   }
+
   bool changed = false;
   for( int i = 0; i < s_count; i++ ) {
-    if( new_readings[i] != last_readings[i] ) { 
-      controlChange(0, (byte)i + 20, new_readings[i]);
-      op[i] = '0' + (int)(new_readings[i] * 10 / 128);
+    if( active[i] > 0 ) { 
+      controlChange(0, channels[i], raw_to_midiCC(new_readings[i]));
+      op[i] = '0' + (int)(new_readings[i] * 10 / 1024);
       changed = true;
     } else {
       op[i] = 'x';
     }
-    last_readings[i] = new_readings[i];
   }
+
+  // write some simple debug
   if( changed ) {
     MidiUSB.flush();
+    Serial.println(op);
   }
-  // send debug
-  //Serial.println(op);
-  delay(10); // delay in between reads for stability
+  // delay 10ms in between reads for stability ~= 100hz
+  // todo use a scheduler
+  // todo read external midi clock
+  delay(10);
 }
 
 void controlChange(byte channel, byte control, byte value) {
